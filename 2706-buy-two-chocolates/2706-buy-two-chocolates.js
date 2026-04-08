43f6e403aa86ab6886d35f81e056f5b6341ede36
snehul v v <snehul81vv@gmail.com>
2026-04-08 23:31:55 +0530

    Time: 3 ms (53.9%), Space: 57 MB (84.42%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+var buyChoco = function(prices, money) {
+   prices.sort((a,b)=>{
+    return a-b;
+   })
+   let sum = prices[0]+prices[1]
+   if(sum<=money){
+    return money-sum
+   }
+   return money
+};
+
+console.log(buyChoco([1,2,2]),3)
