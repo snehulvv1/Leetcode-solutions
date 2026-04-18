41b90e0a897c16938bba07edc388796501dfdff5
snehul v v <snehul81vv@gmail.com>
2026-04-18 21:34:46 +0530

    Time: 43 ms (71.23%), Space: 54 MB (66.49%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -7,10 +7,8 @@
 var reduce = function(nums, fn, init) {
 
   let result = init
-  for(let i=0;i<=nums.length-1;i++){
+  for(let i=0;i<nums.length;i++){
     result = fn(result,nums[i])
   }
   return  result
 };
-
-console.log(reduce([1,2,3,4],function sum(accum, curr) { return accum + curr; },0))
