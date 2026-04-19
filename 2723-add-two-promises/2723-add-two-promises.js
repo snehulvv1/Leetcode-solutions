d2e356b4c78c08ddaf07fcbed0d508f7546e6d5d
snehul v v <snehul81vv@gmail.com>
2026-04-19 22:41:28 +0530

    Time: 52 ms (64.57%), Space: 54.5 MB (39.25%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -4,8 +4,9 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    const [val1,val2] = await Promise.all([promise1,promise2])
-    return val1+val2
+    const firstval = await promise1
+    const secondval = await promise2
+    return firstval+secondval
 };
 
 /**
